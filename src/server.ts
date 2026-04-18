0859fdc372694f7901d5d9d66a1fdc8a7a129b58
kaikecesar <kaikeoliveira196@gmail.com>
2026-04-18 13:16:49 -0300

    feat: add PostgreSQL support and update database connection configuration

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,6 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT }, () => console.log('Server running!'));
+app.listen({ port: env.PORT, host: '0.0.0.0' }, () =>
+  console.log('Server running!'),
+);
